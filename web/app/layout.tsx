71ee2890f5ab4f12316570299d0d13e83f704340
NikhiL34343 <ny9013815499@gmail.com>
2025-11-20 23:03:29 +0530

    layout fixed

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,26 +1,16 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import type { Database } from '@/types/supabase'
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const [supabaseClient] = useState(() => createClientComponentClient<Database>())
-
   return (
     <html lang="en">
-      <body>
-        <SessionContextProvider supabaseClient={supabaseClient}>
-          {children}
-        </SessionContextProvider>
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
 
